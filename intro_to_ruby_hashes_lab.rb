48608644af9ee05717c77eb7e02800df37f21164
Tawhid Ali <tawhidali126@gmail.com>
2019-08-02 13:41:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ def monopoly_with_third_tier
       rent_in_dollars: {
         one_piece_owned: 25,
         two_pieces_owned: 50,
-        third: "300",
+        two_pieces_owned: 50,
         fourth: "400"
       },
       
