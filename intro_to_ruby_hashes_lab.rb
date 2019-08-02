644ce104815ae91fab5ba315ebb0074e3fc8ad19
Tawhid Ali <tawhidali126@gmail.com>
2019-08-02 13:43:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,7 @@ def monopoly_with_third_tier
       },
       
       names: {
-        first: "john",
+        reading_railroad: {},
         second: "bob",
         third: "billy",
         fourth: "sarah"
