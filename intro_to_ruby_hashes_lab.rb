3335d77a090c24342ca1fbce07fab1c4cd5e96b8
Tawhid Ali <tawhidali126@gmail.com>
2019-08-02 14:18:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -41,7 +41,7 @@ def monopoly_with_fourth_tier
         one_piece_owned: 25,
         two_pieces_owned: 50,
         three_pieces_owned: 100,
-        four_pieces_owned: 200
+        four_pieces_owned: 200,
       },
       
       names: {
